7afb8757cbe0d5f9f73159dbc48c7cca2f601669
odutradev <joao.vitornl@gmail.com>
2026-03-04 11:38:42 -0300

    style: Adjusts overflow and side scroll by removing 100vw from the main container

--- src/pages/main/styles.ts
+++ src/pages/main/styles.ts
@@ -2,8 +2,8 @@ import { styled } from '@mui/material/styles';
 import { Box } from '@mui/material';
 
 export const MainContainer = styled(Box)({
-  width: '100vw',
-  height: '100vh',
+  width: '100%',
   display: 'flex',
+  minHeight: '100vh',
   flexDirection: 'column'
 });
